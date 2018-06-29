8ad85a1602d6247f756d1ee964dc842854e8e3b3
Jerico <rickzx98@gmail.com>
2018-06-30 03:24:33 +0800

    updates

--- start-server.bat
+++ start-server.bat
@@ -2,4 +2,4 @@ call create-web.bat
 cd ..
 call create-artifact.bat
 cd ..
-java -jar "./time-service/target/time-service-0.0.1-SNAPSHOT.jar"  -Drun.arguments=--server.port=10000
+java -jar "./time-service/target/time-service-1.0.0.jar"  -Drun.arguments=--server.port=10000
